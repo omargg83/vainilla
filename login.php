a8695f1cdf3d6065a374b5559ecfdf75b6476865
Omar <51249360+omargg83@users.noreply.github.com>
2020-07-23 15:04:33 -0500

    a
    a

--- login.php
+++ login.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="librerias/swal/dist/sweetalert2.min.css">
   </head>
   <body>
     <button class="btn btn-primary" type="button" name="button" onclick='demo()'>Hola mundo</button>
   </body>
+  <script src="librerias/swal/dist/sweetalert2.min.js"></script>
   <script type="text/javascript" src="vainilla.js"></script>
 </html>
--- vainilla.js
+++ vainilla.js
@@ -1,3 +1,20 @@
 function demo(){
   alert("demo");
 }
+
+let numero=5;
+let palabra="Hola mundo";
+let respuesta=true;
+const PI=3.14;
+console.log(numero);
+console.log(palabra);
+console.log(respuesta);
+console.log(PI);
+
+
+/////////////longitud cadena
+console.log(palabra.length);
+console.log(palabra.toUpperCase());
+
+
+Swal.fire('Any fool can use a computer')
